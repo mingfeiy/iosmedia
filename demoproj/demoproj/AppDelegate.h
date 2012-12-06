8e2dc4a29d7ee5f30255c09d66424a4c29154df3
Mingfei Yan <mingfeiy@chadhan-l01.redmond.corp.microsoft.com>
2012-12-06 14:25:59 -0800

    test

---
+++ demoproj/demoproj/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  demoproj
+//
+//  Created by Mingfei Yan on 12/6/12.
+//  Copyright (c) 2012 Mingfei Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ demoproj/demoproj/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  demoproj
+//
+//  Created by Mingfei Yan on 12/6/12.
+//  Copyright (c) 2012 Mingfei Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
